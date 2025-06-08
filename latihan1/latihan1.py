d466185ce99c718f5c38b84ad918439c3d859876
YansaRamaDana <yansaramadana9@gmail.com>
2025-06-08 11:29:56 +0700

    Update latihan1.py

--- latihan1/latihan1.py
+++ latihan1/latihan1.py
@@ -1,6 +1,7 @@
 # =============================================
 # 📌 BELAJAR PYTHON DASAR UNTUK PEMULA
 # =============================================
+# CATATAN : Agar tidak TerRun/Di jalankan semua tambah kan komentar setiap 1 materi. Atas """ ,  Bawah """ 
 # 1. Variabel dan Tipe Data
 # 2. Output dan String Manipulation
 # 3. List dan Tuple
@@ -10,13 +11,17 @@
 # 7. Input dari User
 # 8. Function (Fungsi)
 # 9. Menggunakan Module
-# 10. Mini Project: Konversi Berat
+# 10. Mini Project : Konversi Berat
+# 11. Mini Project : Kalkulator Majemuk
+# 12. Mini Project : Countdown Tahun Baru
+# 13. Perulangan dan Break
+# 14. Mini Project : Time Mundur Detik
 
 
 # ----------------------------
 # 1. VARIABEL DAN TIPE DATA
 # ----------------------------
-nama = 'Yansa Rama Dana'   # String
+nama = 'Selamet Kopling'   # String
 usia = 18                  # Integer
 tinggi_badan = 168.5       # Float
 punya_pacar = True         # Boolean
@@ -33,7 +38,7 @@ print('Usia saya adalah: ' + str(usia) + kata + kata_1 + str(tinggi_badan))
 
 # String Methods
 print(nama.find('R'))
-print(nama.find('Yansa'))
+print(nama.find('Optimus'))
 print(len(nama))
 print('r' in nama)
 print(nama.upper())
@@ -105,7 +110,7 @@ print(f'Halo {nama}, tahun depan kamu akan berusia {usiamu}')
 import os
 os.system('cls')
 
-def sapa(nama):
+def sapa(nama): # nama opsional yahhh 
     print(f'Halo {nama}')
     print('Semangat yah')
     print('Kamu Pasti bisa')
@@ -113,8 +118,6 @@ def sapa(nama):
 sapa('Bagas Titik Kumpull')
 sapa('Daffa Martabak')
 sapa('Agung Buzcut')
-
-# nama Opsional yahh
 
 # ----------------------------
 # 9. MENGGUNAKAN MODULE
@@ -143,3 +146,81 @@ else:
     print(f'{unit} tidak valid')
 
 print(f'Berat kamu adalah: {weight} {unit}')
+
+# ----------------------------
+# 11. MINI PROJECT: KALKULATOR MAJEMUK
+# ----------------------------
+
+# Kalkulator majemuk python
+
+prinsip = 0
+tingkat = 0
+waktu = 0
+
+while True:
+    prinsip = float(input('Masukkan Jumlah Pokok:  '))
+    if prinsip < 0:
+        print('Prinsip tidak bisa kurang dari nol ')
+    else:
+        break
+while True:
+    tingkat = float(input('Masukkan tingkat bunga:  '))
+    if tingkat < 0:
+        print('Tingkat bunga tidak bisa kurang dari nol ')
+    else:
+        break
+while True:
+    waktu = int(input('Masukkan waktu tahun:  '))
+    if waktu < 0:
+        print('Waktu tidak bisa kurang dari nol ')
+    else:
+        break
+
+total = prinsip * pow((1 + tingkat / 100), waktu)
+print(f'Keseimbangan setelahnya {waktu} tahun/s: ${total:.2f}')
+
+
+# ----------------------------
+# 12. MINI PROJECT: COUNTDOWN TAHUN BARU
+# ----------------------------
+
+for x in reversed(range(1, 10)): # Reversed dari bawah ke atas # Range dari atas ke bawah
+    print(x)
+print('HAPPY NEW YEAR!!')
+
+
+# ----------------------------
+# 13. PERULANGAN DAN BREAK
+# ----------------------------
+
+import os
+os.system('cls')
+
+# kartu_kredit = '12434-5678-9012-3456'
+# for x in kartu_kredit:
+    #print(x)
+
+for x in range(1, 21):
+    if x == 13: # Untuk skip
+        break
+    else:
+        print(x)
+
+
+# ----------------------------
+# 14. MINI PROJECT: TIMER MUNDUR DETIK
+# ----------------------------
+
+import time
+
+# Detik Waktu
+time_saya = int(input('Masukkan waktu dalam detik: '))
+
+for  x in range(time_saya, 0, -1):
+    detik = x % 60
+    print(f'00:00:0{detik}')
+    time.sleep(1)
+
+print('TITID BANGAU')
+
+
